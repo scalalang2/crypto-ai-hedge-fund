120f99cce29879662c2b29c499e7b5906c72c825
scalalang2 <cailhuiris@gmail.com>
2025-05-25 20:13:37 +0900

    Added Telegram Login

---
+++ SignInTelegram/Program.cs
@@ -0,0 +1,35 @@
+﻿using CommandLine;
+
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed<Options>(o =>
+    {
+        string Config(string what)
+        {
+            switch (what)
+            {
+                case "api_id": return o.ApiId;
+                case "api_hash": return o.ApiHash;
+                case "phone_number": return o.PhoneNumber;
+                case "verification_code": Console.Write("Code: "); return Console.ReadLine();
+                case "first_name": return "John";      // if sign-up is required
+                case "last_name": return "Doe";        // if sign-up is required
+                case "password": return "secret!";     // if user has enabled 2FA
+                default: return null;                  // let WTelegramClient decide the default config
+            }
+        }
+        
+        using var client = new WTelegram.Client(Config);
+        client.LoginUserIfNeeded().Wait();
+    });
+
+public class Options
+{
+    [Option('a', "api-id", Required = true, HelpText = "Telegram API ID")]
+    public string ApiId { get; set; }
+    
+    [Option('h', "api-hash", Required = true, HelpText = "Telegram API Hash")]
+    public string ApiHash { get; set; } = string.Empty;
+    
+    [Option('p', "phone-number", Required = true, HelpText = "Phone number for Telegram account")]
+    public string PhoneNumber { get; set; } = string.Empty;
+}
---
+++ TradingAgent.Core/Config/TelegramConfig.cs
@@ -0,0 +1,8 @@
+namespace TradingAgent.Core.Config;
+
+public class TelegramConfig
+{
+    public string ApiId { get; set;} = string.Empty;
+    public string ApiHash { get; set;} = string.Empty;
+    public string PhoneNumber { get; set;} = string.Empty;
+}
